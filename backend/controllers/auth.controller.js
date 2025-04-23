ae7a3533b2dc790f2973651ef96b43ae9f1b1f40
Usman <ghaniusman0287@gmail.com>
2025-04-23 21:50:41 +0500

    forget passsword will send a reset link

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import {User} from "../models/user.model.js"
 import bcryptjs from "bcryptjs";
+import crypto from "crypto";
 import { generateTokenAndSetCookie } from "../utilis/generateTokenAndSetCookie.js";
-import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/email.js";
+import { sendPasswordResetEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/email.js";
 
 
 export const signup = async (req,res) => {
@@ -120,4 +121,33 @@ export const login = async (req,res) => {
 export const logout = async (req,res) => {
     res.clearCookie("token");
     res.status(200).json({ success: true, message: "Logged Out Successfully" });
-}
+};
+
+export const forgotPassword = async (req, res) => {
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(400).json({ success: false, message: "User not found" });
+		}
+
+		// Generate reset token
+		const resetToken = crypto.randomBytes(20).toString("hex");
+		const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000; // 1 hour
+
+		user.resetPasswordToken = resetToken;
+		user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+		await user.save();
+
+		// send email
+		await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`);
+
+		res.status(200).json({ success: true, message: "Password reset link sent to your email" });
+	} catch (error) {
+		console.log("Error in forgotPassword ", error);
+		res.status(400).json({ success: false, message: error.message });
+	}
+};
+
